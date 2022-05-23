404309c3e1ffab6f7c9602ce661d1b86c55847ec
park hyunbin <wise7231@gmail.com>
2022-05-23 16:59:08 +0900

    .

--- src/app/User/화장품 추천 모델링/cf.py
+++ src/app/User/화장품 추천 모델링/cf.py
@@ -10,7 +10,8 @@ def getData():
     dir = 'src/app/User/화장품 추천 모델링/최종데이터/'
     df_product = pd.read_csv(dir + 'basic_data_edit.csv', usecols=['00.상품_URL','00.상품코드','01.브랜드','02.상품명','03.가격','04.제품 주요 사양','05.모든 성분','06.총 평점','07.리뷰 개수','08_1.별점 1점','08_2.별점 2점','08_3.별점 3점','08_4.별점 4점','08_5.별점 5점','09_1.피부타입_건성','09_2.피부타입_복합성','09_3.피부타입_지성','10_1.피부고민_보습','10_2.피부고민_진정','10_3.피부고민_주름/미백','11_1.피부자극_없음','11_2.피부자극_보통','11_3.피부자극_있음'],encoding='cp949')
     df_review = pd.read_csv(dir + 'total_review.csv', usecols=['code','user','type','tone','problem','rating','feature','review','total_rating'],encoding='cp949')
-    print(df_review)
+    json_df_review = df_review.to_json()
+    print(json_df_review)
 
 if __name__ == "__main__":
     getData()
